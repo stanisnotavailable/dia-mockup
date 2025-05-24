72a1af005d7e3164f38475557f3ed40922e16b88
stan <stanislav.kostadinov@findmecure.com>
2025-05-24 08:12:28 +0300

    config update

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
+console.log("Mode: ", process.env.NODE_ENV);
 export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
+    ...(process.env.NODE_ENV !== "production" &&
+      process.env.REPL_ID !== undefined
+      ? [
+        await import("@replit/vite-plugin-cartographer").then((m) =>
+          m.cartographer(),
+        ),
+      ]
+      : []),
   ],
   resolve: {
     alias: {
